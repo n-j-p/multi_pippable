f3f28ae57225c00e87f6f1fe71c899185878d52f
n-j-p <10066183+n-j-p@users.noreply.github.com>
2024-12-11 14:16:06 +1100

    Update __init__.py

--- src/multi_pippable/__init__.py
+++ src/multi_pippable/__init__.py
@@ -1,2 +1,6 @@
 from .autoload import *
-import multi_pippable.autoload2
+import multi_pippable.functions
+import multi_pippable.constants
+
+del multi_pippable.autoload
+del multi_pippable.multi_pippable
